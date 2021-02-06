4e5e1c97e84406eccc42e39a416b9153c0a07ac1
bv <avghmn@gmail.com>
2021-02-06 23:42:23 +0300

    refactor: Add pygame initialization

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
+import pygame as pg
 
 from drawing_board import DrawingBoard
 from recognizer import DigitRecognizer
 
 def main():
+    pg.init()
+
     model = tf.keras.models.load_model('models/mnist model')
     digit_recognizer = DigitRecognizer(model)
 
@@ -11,6 +14,8 @@ def main():
     test.create_board()
 
     test.main_loop()
+    
+    pg.quit()
 
 
 if __name__ == '__main__':
